eec1833ce898fad6af9dfbb12f28607a5b062fc7
千葉 勇輝 <yuuki.chiba@fujifilm.com>
2019-10-14 20:39:59 +0900

    vue tutorial: https://jp.vuejs.org/v2/guide/index.html

---
+++ vue-sample/index.js
@@ -0,0 +1,74 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+});
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+});
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+});
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('');
+        }
+    }
+});
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+// todo-item と呼ばれる新しいコンポーネントを定義
+Vue.component('todo-item', {
+    // todo-item コンポーネントはカスタム属性のような "プロパティ" で受け取ります。
+    // このプロパティは todo と呼ばれます。
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+});
+/*
+Vue.component('todo-item', {
+    template: '<li>This is a todo</li>'
+});
+*/
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+    }
+});
